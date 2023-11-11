e039a6949c79c19f75d5f5f4618c4e444854fac5
Michael Richardson <michael.richardson@hey.com>
2023-11-12 11:33:31 +1300

    added terraform to create octopus environment

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+    required_providers {
+        octopusdeploy = {
+          source  = "OctopusDeployLabs/octopusdeploy"
+        }
+        azurerm = {
+          source  = "hashicorp/azurerm"
+          version = "~>3.0"
+        }
+    }
+    backend "azurerm" {
+      resource_group_name  = "shipped23-tfstate"
+      storage_account_name = "tfstateshipped23"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+    }
+}
+
+provider "octopusdeploy" {
+  address   = var.server_url
+  api_key   = var.apiKey
+  space_id  = var.space
+}
+
+data "octopusdeploy_projects" "shipped_project" {
+  name                   = "SHIPPED23"
+}
+
+resource "octopusdeploy_environment" "branch_environment" {
+  name  = "${terraform.workspace}"
+}
+
+resource "octopusdeploy_lifecycle" "branch_lifecycle" {
+  name        = "shipped23_branch_${terraform.workspace}"
+
+  release_retention_policy {
+    quantity_to_keep = 5
+    unit             = "Days"
+  }
+
+  phase {
+    name                            = "${octopusdeploy_environment.branch_environment.name}"
+    automatic_deployment_targets    = ["${octopusdeploy_environment.branch_environment.id}"]
+  }
+
+  depends_on = [octopusdeploy_environment.branch_environment]
+}
+
+resource "octopusdeploy_channel" "branch_channel" {
+  name                  = "${terraform.workspace}"
+  project_id            = "${data.octopusdeploy_projects.shipped_project.id}"
+  lifecycle_id          = "${octopusdeploy_lifecycle.branch_lifecycle.id}"
+}
---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "apiKey" {
+    type = string
+}
+
+variable "space" {
+    type = string
+}
+
+variable "server_url" {
+    type = string
+}
